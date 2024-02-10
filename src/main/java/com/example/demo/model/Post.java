8c9e0b634527972ae540a2d73cafe9e7c0b3aba2
AleksashinPri102 <happywin855@gmail.com>
2024-02-11 00:49:41 +0500

    9.1-9.3

--- src/main/java/com/example/demo/model/Post.java
+++ src/main/java/com/example/demo/model/Post.java
@@ -1,13 +1,22 @@
 package com.example.demo.model;
 
 import java.util.Date;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import java.io.Serializable;
+@Entity
+public class Post implements Serializable {
 
-public class Post {
-
-    private final String text;
     private Integer likes;
+    private String text;
+
+    @Id
+    @GeneratedValue
     private Long id;
     private Date creationDate;
+    public Post() {
+    }
 
     public Post(Long id,String text,Date creationDate) {
         this.text = text;
--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -1,6 +1,8 @@
 package com.example.demo.service;
 
 import com.example.demo.model.Post;
+import com.example.demo.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -12,10 +14,11 @@ public class PostService {
 
     List<Post> posts = new ArrayList<>();
 
-    public List<Post> listAllPosts(){
-        return posts;
+    public Iterable<Post> listAllPosts(){
+        return postRepository.findAll();
     }
-
+    @Autowired
+    PostRepository postRepository;
     {
         posts.add(new Post(1L,"Test1", new Date()));
         posts.add(new Post(2L,"Test2", new Date()));
@@ -24,5 +27,7 @@ public class PostService {
     public void create(String text) {
         long newId = posts.size() + 1;
         posts.add(new Post(newId,text, new Date()));
+        Post post = new Post(null, text, new Date());
+        postRepository.save(post);
     }
 }
